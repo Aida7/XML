60a0694b1beb825220099618883c0b11b4716ef7
Садыкова Аида Хаирбекова <@corp.itstep>
2017-12-22 10:14:43 +0600

    XML

---
+++ XML/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+namespace XML
+{
+    public class Book
+    {
+        public Guid id { get; set; }
+        public string Nmae { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
---
+++ XML/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+namespace XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           XmlDocument document = new XmlDocument();
+            document.Load("Book.xml");
+            //var children =document.ChidNodes;
+            var root = document.DocumentElement;
+            XmlElement element = document.CreateElement("book");
+            element.SetAttribute("id", "34-03432324");
+            root.AppendChild(element);
+         
+            foreach (XmlNode child in root.ChildNodes)
+            {
+                Book book = new Book();
+                book.id = Guid.Parse(child.Attributes["Id"].InnerText);
+            }
+        }
+    }
+}
